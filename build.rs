97a4faa1531a5f0abee744383b97b3b243aaacfc
Ben Zhang <benzh@cs.berkeley.edu>
2016-08-25 09:01:13 -0700

    fix(build): add include path; update lib path

--- build.rs
+++ build.rs
@@ -4,11 +4,12 @@ fn main() {
     gcc::Config::new()
         .cpp(true)
         .file("native/opencv-wrapper.cc")
+	.include("/usr/local/include")
         .include("native")
         .flag("--std=c++11")
         .compile("libopencv-wrapper.a");
 
-    println!("cargo:rustc-link-search=native=-L/usr/local/lib");
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
     println!("cargo:rustc-link-lib=opencv_core");
     println!("cargo:rustc-link-lib=opencv_imgcodecs");
     println!("cargo:rustc-link-lib=opencv_imgproc");
