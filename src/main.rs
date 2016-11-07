06a835079f14af56d9d68d4fbbed41e4a2cddb2a
Ben Zhang <benzh@cs.berkeley.edu>
2016-11-06 23:00:45 -0800

    feat(ex): hog examples now runs smoothly

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,71 @@
-// This resembles the OpenCV read image example code:
-// http://docs.opencv.org/3.1.0/db/deb/tutorial_display_image.html
+extern crate getopts;
 extern crate rust_vision;
+
 use rust_vision::*;
 use rust_vision::cuda::*;
 use rust_vision::objdetect::SvmDetector;
+use std::fs;
+use std::fs::File;
+use std::io::{Read, Result};
+use std::path::Path;
 
 fn main() {
-    let mat = Mat::from_path("./assets/AVG-TownCentre-test-000011.jpg", 0);
+    run().unwrap();
+}
+
+fn run() -> Result<()> {
+    let args: Vec<String> = ::std::env::args().collect();
+    let program = args[0].clone();
+    let mut opts = getopts::Options::new();
+    opts.optopt("d", "", "set the directory to look for images", "DIRECTORY");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(_) => {
+            print_usage(&program, opts);
+            ::std::process::exit(-1);
+        }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return Ok(());
+    }
+
+    let dir = matches.opt_str("d").expect("You need to provide the directory");
+    let path = Path::new(&dir);
+    for entry in try!(fs::read_dir(path)) {
+        let dir = try!(entry);
+        println!("{:?}", dir.path());
+
+        let mut hog = GpuHog::default();
+        let detector = SvmDetector::default_people_detector();
+        hog.set_svm_detector(detector);
+        run_hog_for_path(&mut hog, dir.path());
+    }
+    Ok(())
+}
+
+fn run_hog_for_path<P: AsRef<Path>>(hog: &mut GpuHog, path: P) {
+    let mut buf = Vec::new();
+    File::open(path).unwrap().read_to_end(&mut buf).unwrap();
+    let mat = Mat::imdecode(&buf, ImreadModes::ImreadGrayscale);
     let mut gpu_mat = GpuMat::default();
     gpu_mat.upload(&mat);
+    let start = ::std::time::Instant::now();
+    let results = hog.detect(&gpu_mat);
+    let elapsed = start.elapsed();
+    println!("{} ms",
+             elapsed.as_secs() as f64 * 1_000.0 + elapsed.subsec_nanos() as f64 / 1_000_000.0);
 
-    let detector = SvmDetector::default_people_detector();
-    let mut hog = GpuHog::default();
-    hog.set_svm_detector(detector);
-    let found = hog.detect(&gpu_mat);
-
-    found.iter()
+    highgui_named_window("window", WindowFlags::WindowAutosize);
+    results.iter()
         .map(|&r| mat.rectangle(r.scale(0.6)))
         .count();
-    highgui_named_window("Display window", WindowFlags::WindowAutosize);
-    mat.show("Display window", 0);
+    mat.show("window", 0);
+}
+
+fn print_usage(program: &str, opts: getopts::Options) {
+    let brief = format!("Usage: {} [options] DIRECTORY", program);
+    print!("{}", opts.usage(&brief));
 }
