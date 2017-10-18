d6de79f842acaccbc5a5185a306ace7a734f37a3
Ben Zhang <benzh@cs.berkeley.edu>
2017-10-17 22:59:14 -0700

    windows link against opencv_world320

--- build.rs
+++ build.rs
@@ -11,9 +11,10 @@ fn opencv_include() -> String {
 }
 
 #[cfg(windows)]
-fn opencv_lib() -> String {
+fn opencv_link() {
     if let Ok(dir) = std::env::var("OPENCV_LIB") {
-        dir
+        println!("cargo:rustc-link-search=native={}", dir);
+        println!("cargo:rustc-link-lib=opencv_world320");
     } else {
         eprint!("%OPENCV_DIR% is not set properly.");
         std::process::exit(0x0100);
@@ -26,8 +27,19 @@ fn opencv_include() -> &'static str {
 }
 
 #[cfg(unix)]
-fn opencv_lib() -> &'static str {
-    "/usr/local/lib"
+fn opencv_link() {
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
+    println!("cargo:rustc-link-lib=opencv_core");
+    println!("cargo:rustc-link-lib=opencv_imgcodecs");
+    println!("cargo:rustc-link-lib=opencv_imgproc");
+    println!("cargo:rustc-link-lib=opencv_highgui");
+    println!("cargo:rustc-link-lib=opencv_videoio");
+    println!("cargo:rustc-link-lib=opencv_objdetect");
+    println!("cargo:rustc-link-lib=opencv_video");
+
+    if cfg!(feature = "gpu") {
+        println!("cargo:rustc-link-lib=opencv_cudaobjdetect");
+    }
 }
 
 fn main() {
@@ -44,17 +56,5 @@ fn main() {
     }
 
     opencv_config.compile("libopencv-wrapper.a");
-
-    println!("cargo:rustc-link-search=native={}", opencv_lib());
-    println!("cargo:rustc-link-lib=opencv_core");
-    println!("cargo:rustc-link-lib=opencv_imgcodecs");
-    println!("cargo:rustc-link-lib=opencv_imgproc");
-    println!("cargo:rustc-link-lib=opencv_highgui");
-    println!("cargo:rustc-link-lib=opencv_videoio");
-    println!("cargo:rustc-link-lib=opencv_objdetect");
-    println!("cargo:rustc-link-lib=opencv_video");
-
-    if cfg!(feature = "gpu") {
-        println!("cargo:rustc-link-lib=opencv_cudaobjdetect");
-    }
+    opencv_link();
 }
