3d1c04a8ed05992493af1fd43cbb4355cbf3ce2d
Psilon <pzixel@gmail.com>
2018-01-14 21:26:04 +0300

    2 attempt to fix the issue

---
+++ .ci/appveyor_install_OCV.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+myRepo=$(pwd)
+CMAKE_CONFIG_GENERATOR="Visual Studio 15 2017 Win64"
+if [  ! -d "$myRepo/opencv"  ]; then
+    echo "clonning opencv"
+    git clone https://github.com/opencv/opencv.git
+    mkdir -p Build
+    mkdir -p Build/opencv
+    mkdir -p Install
+    mkdir -p Install/opencv
+else
+    cd opencv
+    git pull --rebase
+    cd ..
+fi
+if [  ! -d "$myRepo/opencv_contrib"  ]; then
+    echo "clonning opencv_contrib"
+    git clone https://github.com/opencv/opencv_contrib.git
+    mkdir -p Build
+    mkdir -p Build/opencv_contrib
+else
+    cd opencv_contrib
+    git pull --rebase
+    cd ..
+fi
+RepoSource=opencv
+pushd Build/$RepoSource
+CMAKE_OPTIONS='-DBUILD_PERF_TESTS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DBUILD_DOCS:BOOL=OFF  -DWITH_CUDA:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DINSTALL_CREATE_DISTRIB=ON'
+cmake -G"$CMAKE_CONFIG_GENERATOR" $CMAKE_OPTIONS -DOPENCV_EXTRA_MODULES_PATH="$myRepo"/opencv_contrib/modules -DCMAKE_INSTALL_PREFIX="$myRepo"/install/"$RepoSource" "$myRepo/$RepoSource"
+echo "************************* $Source_DIR -->release"
+cmake --build .  --target install --config release
+popd
