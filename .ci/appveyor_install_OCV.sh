c573f073b527a9670f6326b527d28cd8ff097997
Psilon <pzixel@gmail.com>
2018-01-14 21:45:02 +0300

    AppVeyor plays

--- .ci/appveyor_install_OCV.sh
+++ .ci/appveyor_install_OCV.sh
@@ -18,7 +18,7 @@ else
     cd ..
 fi
 RepoSource=opencv
-pushd Build/$RepoSource
+pushd build/$RepoSource
 CMAKE_OPTIONS='-DBUILD_PERF_TESTS:BOOL=OFF -DBUILD_TESTS:BOOL=OFF -DBUILD_DOCS:BOOL=OFF  -DWITH_CUDA:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DINSTALL_CREATE_DISTRIB=ON'
 cmake -G"$CMAKE_CONFIG_GENERATOR" $CMAKE_OPTIONS -DOPENCV_EXTRA_MODULES_PATH="$myRepo"/opencv_contrib/modules -DCMAKE_INSTALL_PREFIX="$myRepo"/install/"$RepoSource" "$myRepo/$RepoSource"
 echo "************************* $Source_DIR -->release"
