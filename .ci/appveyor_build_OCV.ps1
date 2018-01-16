460fa2f9dbeb485290b6faa680e79e5a6025070c
pzixel <pzixel@gmail.com>
2018-01-16 19:18:13 +0300

    appveyor plays

--- .ci/appveyor_build_OCV.ps1
+++ .ci/appveyor_build_OCV.ps1
@@ -1,5 +1,6 @@
 #SCRIPT CONSTANTS
 $pwd = Get-Location
+$REPO_LOCATION = "$pwd\opencv"
 $OPENCV_DIR = "$pwd\install\opencv";
 $OPENCV_VERSION_TAG = "3.4.0"
 $CMAKE_CONFIG_GENERATOR = "Visual Studio 15 2017 Win64"
@@ -17,8 +18,6 @@ $CMAKE_OPTIONS = @(
   "-DBUILD_EXAMPLES:BOOL=OFF",
   "-DINSTALL_CREATE_DISTRIB:BOOL=ON"
 )
-$INSTALL_LOCATION = "$pwd\install\opencv"
-$REPO_LOCATION = "$pwd\opencv"
 
 #SCRIPT BODY
 Write-Host "CONFIGURE OPENCV PATHS"
@@ -36,7 +35,7 @@ if (Test-Path "$OPENCV_DIR\x64\vc15\bin") {
 	Write-Host "Compiled OpenCV found. Skip installation"
 	return;
 }
-Write-Host "INSTALL OPENCV AT $INSTALL_LOCATION"
+Write-Host "INSTALL OPENCV AT $OPENCV_DIR"
 
 mkdir build\opencv -ErrorAction SilentlyContinue
 mkdir install\opencv -ErrorAction SilentlyContinue
@@ -47,8 +46,8 @@ git clone -b $OPENCV_VERSION_TAG --depth 1 https://github.com/opencv/opencv.git
 $ErrorActionPreference = $oldErrorAction
 
 Push-Location -Path "build\opencv"
-Write-Host "cmake -G $CMAKE_CONFIG_GENERATOR -DCMAKE_INSTALL_PREFIX=$INSTALL_LOCATION $REPO_LOCATION $CMAKE_OPTIONS"
-cmake -G $CMAKE_CONFIG_GENERATOR "-DCMAKE_INSTALL_PREFIX=$INSTALL_LOCATION" $REPO_LOCATION @CMAKE_OPTIONS
+Write-Host "cmake -G $CMAKE_CONFIG_GENERATOR -DCMAKE_INSTALL_PREFIX=$OPENCV_DIR $REPO_LOCATION $CMAKE_OPTIONS"
+cmake -G $CMAKE_CONFIG_GENERATOR "-DCMAKE_INSTALL_PREFIX=$OPENCV_DIR" $REPO_LOCATION @CMAKE_OPTIONS
 if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
 cmake --build .  --target install --config release
 if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
