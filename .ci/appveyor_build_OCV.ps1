f769be55f7b66925ac546f734ed12a8a13bb5c3c
pzixel <pzixel@gmail.com>
2018-01-16 17:54:10 +0300

    appveyor plays

--- .ci/appveyor_build_OCV.ps1
+++ .ci/appveyor_build_OCV.ps1
@@ -38,8 +38,7 @@ Write-Host "INSTALL OPENCV AT $INSTALL_LOCATION"
 
 mkdir build\opencv -ErrorAction SilentlyContinue
 mkdir install\opencv -ErrorAction SilentlyContinue
-
-git clone -b $OPENCV_VERSION_TAG --depth 1 https://github.com/opencv/opencv.git 2>&1 | % { $_.ToString() }
+(cmd /c git clone -b $OPENCV_VERSION_TAG --depth 1 https://github.com/opencv/opencv.git 2>&1)
 
 Push-Location -Path "build\opencv"
 Write-Host "cmake -G $CMAKE_CONFIG_GENERATOR -DCMAKE_INSTALL_PREFIX=$INSTALL_LOCATION $REPO_LOCATION $CMAKE_OPTIONS"
