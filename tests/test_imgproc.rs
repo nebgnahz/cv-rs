8876da16205b1a29c3ce5548343ad8b78ee7f928
pzixel <pzixel@gmail.com>
2018-01-25 17:44:39 +0300

    compare_hist

--- tests/test_imgproc.rs
+++ tests/test_imgproc.rs
@@ -1,13 +1,27 @@
 extern crate cv;
 mod utils;
 
+use cv::*;
 use cv::imgproc::*;
 use utils::*;
 
 #[test]
 fn compare_hist() {
-    let first_image = load_unchanged("assets/Histogram_Comparison_Source_0.jpg");
-    let second_image = load_unchanged("assets/Histogram_Comparison_Source_1.jpg");
-    let result = first_image.compare_hist(&second_image, HistogramComparisionMethod::ChiSquare).unwrap();
-    assert_eq!(0f64, result);
+    let first_image = get_image_histogram("assets/Histogram_Comparison_Source_0.jpg");
+    let second_image = get_image_histogram("assets/Histogram_Comparison_Source_1.jpg");
+    let result = first_image.compare_hist(&second_image, HistogramComparisionMethod::Corellation).unwrap();
+    assert!((result - 0.211).abs() < 0.001);
+}
+
+fn get_image_histogram(path: &'static str) -> Mat {
+    let image = load_unchanged(path);
+    let image = image.cvt_color(ColorConversionCodes::BGR2HSV);
+    let hsize = [50, 60];
+    let h_ranges = [0_f32, 180_f32];
+    let s_ranges = [0_f32, 256_f32];
+    let ranges = [h_ranges.as_ptr() as *const f32, s_ranges.as_ptr() as *const f32];
+    let channels = [0, 1];
+    let image = image.calc_hist(channels.as_ptr(), Mat::new(), 2, hsize.as_ptr(), ranges.as_ptr());
+    let image = image.normalize(0_f64, 1_f64, NormTypes::NormMinMax);
+    image
 }
