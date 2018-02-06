590b23edc4530c189bfc9f0f7aa7c646101ad25e
pzixel <pzixel@gmail.com>
2018-02-06 16:11:52 +0300

    clang-format

--- setup_hooks.sh
+++ setup_hooks.sh
@@ -3,7 +3,9 @@ rustup component add rustfmt-preview
 
 rustfmt_path=`find $HOME/.rustup/toolchains/ -wholename '*bin/rustfmt*'`
 echo "#!/bin/sh
-find . -name *.rs -type f -print | xargs $rustfmt_path"  > .git/hooks/pre-commit
+find . -name '*.rs' -type f | xargs $rustfmt_path &
+find native/ -type f | xargs clang-format -i &
+wait"  > .git/hooks/pre-commit
 
 chmod +x .git/hooks/pre-commit
 
