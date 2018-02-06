4512b5422cf04f29161ec90b5bf83e839714d593
pzixel <pzixel@gmail.com>
2018-02-06 17:23:30 +0300

    Run on changed files only

--- setup_hooks.sh
+++ setup_hooks.sh
@@ -3,8 +3,19 @@ rustup component add rustfmt-preview
 
 rustfmt_path=`find $HOME/.rustup/toolchains/ -wholename '*bin/rustfmt*'`
 echo "#!/bin/sh
-find . -name '*.rs' -type f | xargs $rustfmt_path &
-find native/ -type f | xargs clang-format -i &
+declare -a rust_files=()
+declare -a cpp_files=()
+
+for file in \$(git ls-files -m); do
+    if [[ \"\${file}\" == *.rs ]]; then
+        rust_files+=(\"\${file}\")
+    fi
+    if [[ \"\${file}\" =~ (\.h|\.cpp|\.cc) ]]; then
+        cpp_files+=(\"\${file}\")
+    fi
+done
+echo \${rust_files[@]} | xargs --no-run-if-empty $rustfmt_path &
+echo \${cpp_files[@]} | xargs --no-run-if-empty clang-format -i &
 wait"  > .git/hooks/pre-commit
 
 chmod +x .git/hooks/pre-commit
