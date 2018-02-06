2f87024ed46ec5e633414478608dd4c356f2caae
pzixel <pzixel@gmail.com>
2018-02-06 18:35:22 +0300

    updated script

--- setup_hooks.sh
+++ setup_hooks.sh
@@ -6,7 +6,9 @@ echo "#!/bin/sh
 declare -a rust_files=()
 declare -a cpp_files=()
 
-for file in \$(git ls-files -m -o --exclude-standard); do
+files=\$(git diff-index --name-only HEAD)
+echo 'Formatting source files'
+for file in \$files; do
     if [ ! -f \"\${file}\" ]; then
         continue
     fi
@@ -19,7 +21,15 @@ for file in \$(git ls-files -m -o --exclude-standard); do
 done
 echo \${rust_files[@]} | xargs --no-run-if-empty $rustfmt_path &
 echo \${cpp_files[@]} | xargs --no-run-if-empty clang-format -i &
-wait"  > .git/hooks/pre-commit
+wait
+
+changed_files=(\"\${rust_files[@]}\" \"\${cpp_files[@]}\")
+if [ \${#changed_files[@]} -ne 0 ]; then
+    git add \${changed_files[@]}
+	echo \"Formatting done, changed files: \${changed_files[@]}\"
+else
+	echo \"No changes, formatting skipped\"
+fi"  > .git/hooks/pre-commit
 
 chmod +x .git/hooks/pre-commit
 
