f61ef9edabbd39e61b1782d557616463dd610c94
Psilon <pzixel@gmail.com>
2018-02-12 17:40:40 +0300

    rust side (early and buggy)

--- src/text/mod.rs
+++ src/text/mod.rs
@@ -1,3 +1,3 @@
-//! Provide 2D image feature detectors and descriptor extractors
+//! Provides different algorithms for text detection and recognition in natural scene images
 mod ocr;
 pub use self::ocr::*;
