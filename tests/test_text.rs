6a7ce82dbfae76a71557304d87921bad6e060a40
Pzixel <pzixel@gmail.com>
2018-02-13 16:35:59 +0300

    segfault fix

--- tests/test_text.rs
+++ tests/test_text.rs
@@ -7,25 +7,39 @@ use cv::imgproc::*;
 use cv::text::*;
 use std::path::PathBuf;
 
-//#[test]
-//fn ocr_tesseract_test() {
-//    let image = Mat::from_path("assets/HelloWorld.png", ImreadModes::ImreadColor).unwrap();
-//    let path = PathBuf::from("/usr/share/tesseract-ocr");
-//    let vec = vec!['z' as std::os::raw::c_char];
-//    let ocr = OcrTesseract::new(
-//        Some(&path),
-//        Some("eng"),
-//        Some(&vec),
-//        EngineMode::Default,
-//        PageSegmentationMode::Auto,
-//    );
-//    let res = ocr.run(&image, ComponentLevel::TextLine);
-//    assert!(res.0.contains("Heruro worudo"));
-//}
+#[test]
+fn ocr_tesseract_test_line() {
+    let image = Mat::from_path("assets/HelloWorld.png", ImreadModes::ImreadColor).unwrap();
+    let path = PathBuf::from("/usr/share/tesseract-ocr");
+    let ocr = OcrTesseract::new(
+        Some(&path),
+        Some("eng"),
+        None,
+        EngineMode::Default,
+        PageSegmentationMode::Auto,
+    );
+    let res = ocr.run(&image, ComponentLevel::TextLine);
+    assert_contains(&res.0, "Heruro worudo")
+}
+
+#[test]
+fn ocr_tesseract_test_word() {
+    let image = Mat::from_path("assets/Ubuntu.png", ImreadModes::ImreadColor).unwrap();
+    let path = PathBuf::from("/usr/share/tesseract-ocr");
+    let ocr = OcrTesseract::new(
+        Some(&path),
+        Some("eng"),
+        None,
+        EngineMode::Default,
+        PageSegmentationMode::Auto,
+    );
+    let res = ocr.run(&image, ComponentLevel::Word);
+    assert_contains(&res.0, "uBuntu")
+}
 
 #[test]
 fn ocr_hmm_test() {
-    let image = Mat::from_path("assets/HelloWorld.png", ImreadModes::ImreadColor).unwrap();
+    let image = Mat::from_path("assets/Ubuntu.png", ImreadModes::ImreadColor).unwrap();
     let image = image.cvt_color(ColorConversionCodes::BGR2GRAY);
     let classifier_name = PathBuf::from("assets/OCRHMM_knn_model_data.xml.gz");
     let transition_probability_path = PathBuf::from("assets/OCRHMM_transitions_table.xml");
@@ -39,5 +53,9 @@ fn ocr_hmm_test() {
         &emission_probability_table,
     ).unwrap();
     let res = ocr.run(&image, ComponentLevel::Word);
-    assert!(res.0.contains("Heruro worudo"));
+    assert_contains(&res.0, "uBuntu");
+}
+
+fn assert_contains(left: &str, right: &str) {
+    assert!(left.contains(right), "{} != {}", left, right);
 }
