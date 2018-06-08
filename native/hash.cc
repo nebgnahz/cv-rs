291ef17a5039987d84460813c86c7b725f5349e9
Psilon <pzixel@gmail.com>
2018-06-08 23:16:08 +0300

    some stuff

--- native/hash.cc
+++ native/hash.cc
@@ -3,21 +3,67 @@
 
 extern "C" {
 
+void cv_hash_compute(cv::Ptr<cv::img_hash::PHash>* hash, cv::Mat& mat, cv::Mat& result) {
+    hash->get()->compute(mat, result);
+}
+
+double cv_hash_compare(cv::Ptr<cv::img_hash::PHash>* hash, cv::Mat& lhs, cv::Mat& rhs) {
+    return hash->get()->compare(lhs, rhs);
+}
+
+void* cv_average_hash_new() {
+    cv::Ptr<cv::img_hash::AverageHash> result = cv::img_hash::AverageHash::create();
+    return new cv::Ptr<cv::img_hash::AverageHash>(result);
+}
+void cv_average_hash_drop(cv::Ptr<cv::img_hash::AverageHash>* hash) {
+    delete hash;
+    hash = nullptr;
+}
+
+void* cv_block_mean_hash_new() {
+    cv::Ptr<cv::img_hash::BlockMeanHash> result = cv::img_hash::BlockMeanHash::create();
+    return new cv::Ptr<cv::img_hash::BlockMeanHash>(result);
+}
+void cv_block_mean_hash_drop(cv::Ptr<cv::img_hash::BlockMeanHash>* hash) {
+    delete hash;
+    hash = nullptr;
+}
+
+void* cv_color_moment_hash_new() {
+    cv::Ptr<cv::img_hash::ColorMomentHash> result = cv::img_hash::ColorMomentHash::create();
+    return new cv::Ptr<cv::img_hash::ColorMomentHash>(result);
+}
+void cv_color_moment_hash_drop(cv::Ptr<cv::img_hash::ColorMomentHash>* hash) {
+    delete hash;
+    hash = nullptr;
+}
+
+void* cv_marr_hildreth_hash_new() {
+    cv::Ptr<cv::img_hash::MarrHildrethHash> result = cv::img_hash::MarrHildrethHash::create();
+    return new cv::Ptr<cv::img_hash::MarrHildrethHash>(result);
+}
+void cv_marr_hildreth_hash_drop(cv::Ptr<cv::img_hash::MarrHildrethHash>* hash) {
+    delete hash;
+    hash = nullptr;
+}
+
 void* cv_phash_new() {
     cv::Ptr<cv::img_hash::PHash> result = cv::img_hash::PHash::create();
     return new cv::Ptr<cv::img_hash::PHash>(result);
 }
 
-void cv_phash_drop(cv::Ptr<cv::img_hash::PHash>* phash) {
-    delete phash;
-    phash = nullptr;
+void cv_phash_drop(cv::Ptr<cv::img_hash::PHash>* hash) {
+    delete hash;
+    hash = nullptr;
 }
 
-void cv_hash_compute(cv::Ptr<cv::img_hash::PHash>* phash, cv::Mat& mat, cv::Mat& result) {
-    phash->get()->compute(mat, result);
+void* cv_radial_variance_hash_new() {
+    cv::Ptr<cv::img_hash::RadialVarianceHash> result = cv::img_hash::RadialVarianceHash::create();
+    return new cv::Ptr<cv::img_hash::RadialVarianceHash>(result);
 }
 
-double cv_hash_compare(cv::Ptr<cv::img_hash::PHash>* phash, cv::Mat& lhs, cv::Mat& rhs) {
-    return phash->get()->compare(lhs, rhs);
+void cv_radial_variance_hash_drop(cv::Ptr<cv::img_hash::RadialVarianceHash>* hash) {
+    delete hash;
+    hash = nullptr;
 }
 }
--- native/hash.h
+++ native/hash.h
@@ -6,9 +6,25 @@
 
 extern "C" {
 
+void cv_hash_compute(cv::Ptr<cv::img_hash::ImgHashBase>* phash, cv::Mat& mat, cv::Mat& result);
+void cv_hash_compare(cv::Ptr<cv::img_hash::ImgHashBase>* phash, cv::Mat& lhs, cv::Mat& rhs);
+
+void* cv_average_hash_new();
+void cv_average_hash_drop(cv::Ptr<cv::img_hash::AverageHash>* phash);
+
+void* cv_block_mean_hash_new();
+void cv_block_mean_hash_drop(cv::Ptr<cv::img_hash::BlockMeanHash>* phash);
+
+void* cv_color_moment_hash_new();
+void cv_color_moment_hash_drop(cv::Ptr<cv::img_hash::ColorMomentHash>* phash);
+
+void* cv_marr_hildreth_hash_new();
+void cv_marr_hildreth_hash_drop(cv::Ptr<cv::img_hash::MarrHildrethHash>* phash);
+
 void* cv_phash_new();
 void cv_phash_drop(cv::Ptr<cv::img_hash::PHash>* phash);
-void cv_hash_compute(cv::Ptr<cv::img_hash::PHash>* phash, cv::Mat& mat, cv::Mat& result);
-void cv_hash_compare(cv::Ptr<cv::img_hash::PHash>* phash, cv::Mat& lhs, cv::Mat& rhs);
+
+void* cv_radial_variance_hash_new();
+void cv_radial_variance_hash_drop(cv::Ptr<cv::img_hash::RadialVarianceHash>* phash);
 }
 #endif
