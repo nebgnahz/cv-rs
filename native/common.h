746c3c71d9a1b322f59d8bff24b454bd44661101
Nicola Orrù <nicola_orru@scee.net>
2018-10-05 17:08:32 +0100

    Reverted file that didn't need to be formatted

--- native/common.h
+++ native/common.h
@@ -61,7 +61,9 @@ typedef struct {
     int trainIdx;
 } DMatch;
 
-typedef struct { const char* value; } CDisposableString;
+typedef struct {
+    const char* value;
+} CDisposableString;
 
 // Caller is responsible for disposing `error` field
 template <typename T>
