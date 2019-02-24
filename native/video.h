5a90fef41f362d1de52c6a01dfcdfa34ba222746
Yuval Langer <yuval.langer@gmail.com>
2019-02-24 17:51:32 +0200

    Use clang-format.

--- native/video.h
+++ native/video.h
@@ -2,61 +2,44 @@
 #define CV_RS_VIDEO_H
 
 #include "common.h"
-#include <opencv2/video/tracking.hpp>
 #include <opencv2/video/background_segm.hpp>
+#include <opencv2/video/tracking.hpp>
 
 extern "C" {
-    void* cv_term_criteria_new(int type, int count, double epsilon);
-    void cv_term_criteria_drop(cv::TermCriteria* criteria);
-    RotatedRect cv_camshift(cv::Mat* back_project_image, Rect window, cv::TermCriteria* criteria);
+void* cv_term_criteria_new(int type, int count, double epsilon);
+void cv_term_criteria_drop(cv::TermCriteria* criteria);
+RotatedRect cv_camshift(cv::Mat* back_project_image, Rect window, cv::TermCriteria* criteria);
 
-    // BackgroundSubtractorKNN
+// BackgroundSubtractorKNN
 
-    void* cv_create_background_subtractor_knn(
-        int history,
-        double dist2Threshold,
-        bool detectShadows
-    );
+void* cv_create_background_subtractor_knn(int history, double dist2Threshold, bool detectShadows);
 
-    void* cv_create_background_subtractor_knn_default();
+void* cv_create_background_subtractor_knn_default();
 
-    void cv_background_subtractor_knn_drop(cv::Ptr<cv::BackgroundSubtractorKNN>* background_subtractor);
+void cv_background_subtractor_knn_drop(cv::Ptr<cv::BackgroundSubtractorKNN>* background_subtractor);
 
-    void cv_background_subtractor_knn_apply(
-        cv::Ptr<cv::BackgroundSubtractorKNN> background_subtractor,
-        cv::Mat* input_image,
-        cv::Mat* output_foreground,
-        double learning_rate
-    );
+void cv_background_subtractor_knn_apply(cv::Ptr<cv::BackgroundSubtractorKNN> background_subtractor,
+                                        cv::Mat* input_image,
+                                        cv::Mat* output_foreground,
+                                        double learning_rate);
 
-    void cv_background_subtractor_knn_get_background_image(
-        cv::Ptr<cv::BackgroundSubtractorKNN> background_subtractor,
-        cv::Mat* output_background
-    );
+void cv_background_subtractor_knn_get_background_image(cv::Ptr<cv::BackgroundSubtractorKNN> background_subtractor,
+                                                       cv::Mat* output_background);
 
-    // BackgroundSubtractorMOG2
+// BackgroundSubtractorMOG2
 
-    void* cv_create_background_subtractor_mog2(
-        int history,
-        double dist2Threshold,
-        bool detectShadows
-    );
+void* cv_create_background_subtractor_mog2(int history, double dist2Threshold, bool detectShadows);
 
-    void* cv_create_background_subtractor_mog2_default();
+void* cv_create_background_subtractor_mog2_default();
 
-    void cv_background_subtractor_mog2_drop(cv::Ptr<cv::BackgroundSubtractorMOG2>* background_subtractor);
+void cv_background_subtractor_mog2_drop(cv::Ptr<cv::BackgroundSubtractorMOG2>* background_subtractor);
 
-    void cv_background_subtractor_mog2_apply(
-        cv::Ptr<cv::BackgroundSubtractorMOG2> background_subtractor,
-        cv::Mat* input_image,
-        cv::Mat* output_foreground,
-        double learning_rate
-    );
+void cv_background_subtractor_mog2_apply(cv::Ptr<cv::BackgroundSubtractorMOG2> background_subtractor,
+                                         cv::Mat* input_image,
+                                         cv::Mat* output_foreground,
+                                         double learning_rate);
 
-    void cv_background_subtractor_mog2_get_background_image(
-        cv::Ptr<cv::BackgroundSubtractorMOG2> background_subtractor,
-        cv::Mat* output_background
-    );
-
+void cv_background_subtractor_mog2_get_background_image(cv::Ptr<cv::BackgroundSubtractorMOG2> background_subtractor,
+                                                        cv::Mat* output_background);
 }
 #endif  // CV_RS_VIDEO_H
