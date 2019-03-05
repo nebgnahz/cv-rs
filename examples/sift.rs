31063f962c81d2a7829a485663f3603621a82ce7
Geordon Worley <vadixidav@gmail.com>
2019-03-05 14:11:42 -0500

    change sift example to use ellipse

--- examples/sift.rs
+++ examples/sift.rs
@@ -4,6 +4,8 @@ use cv::features2d::*;
 use cv::highgui::*;
 use cv::imgcodecs::ImageReadMode;
 use cv::*;
+use std::f64::consts::PI;
+use std::os::raw::c_int;
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
@@ -12,7 +14,7 @@ fn main() {
         std::process::exit(-1);
     }
 
-    let mat = Mat::from_path(&args[1], ImageReadMode::AnyColor).expect("Failed to read from path");
+    let mat = Mat::from_path(&args[1], ImageReadMode::Color).expect("Failed to read from path");
 
     if !mat.is_valid() {
         println!("Could not open or find the image");
@@ -22,13 +24,18 @@ fn main() {
     let mask = Mat::new();
     let sift: SIFT = SIFTBuilder::default().into();
     let (keypoints, _) = sift.detect_and_compute(&mat, &mask);
-    let mut data = mat.data().to_vec();
+
+    let draw_mat = mat.clone();
     for kp in keypoints {
-        let x = kp.point.x as usize;
-        let y = kp.point.y as usize;
-        data[mat.elem_size() * (x + y * mat.size().width as usize) + 1] = 255;
+        let x = kp.point.x as c_int;
+        let y = kp.point.y as c_int;
+        draw_mat.ellipse(
+            Point2i::new(x, y),
+            Size2i::new((kp.size * 1.0).ceil() as i32, (kp.size * 0.5).ceil() as i32),
+            f64::from(kp.angle),
+            0.0,
+            360.0,
+        );
     }
-    Mat::from_buffer(mat.rows, mat.cols, mat.cv_type(), &data)
-        .show("SIFT Points", 0)
-        .unwrap();
+    draw_mat.show("SIFT Points", 0).unwrap();
 }
