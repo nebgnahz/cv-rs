6357117bedaed779f67b9005854e43cb4cd96f38
Geordon Worley <vadixidav@gmail.com>
2019-03-05 15:11:04 -0500

    add different color channels to sift example

--- examples/sift.rs
+++ examples/sift.rs
@@ -1,10 +1,13 @@
 extern crate cv;
+#[macro_use]
+extern crate itertools;
 
 use cv::features2d::*;
 use cv::highgui::*;
 use cv::imgcodecs::ImageReadMode;
 use cv::*;
-use std::f64::consts::PI;
+use itertools::Itertools;
+use std::iter::once;
 use std::os::raw::c_int;
 
 fn main() {
@@ -21,21 +24,48 @@ fn main() {
         std::process::exit(-1);
     }
 
+    let blue = mat.mix_channels(1, 1, [(0, 0)]);
+    let green = mat.mix_channels(1, 1, [(1, 0)]);
+    let red = mat.mix_channels(1, 1, [(2, 0)]);
+
     let mask = Mat::new();
     let sift: SIFT = SIFTBuilder::default().into();
-    let (keypoints, _) = sift.detect_and_compute(&mat, &mask);
+    let (red_keypoints, _) = sift.detect_and_compute(&red, &mask);
+    let (green_keypoints, _) = sift.detect_and_compute(&green, &mask);
+    let (blue_keypoints, _) = sift.detect_and_compute(&blue, &mask);
 
-    let draw_mat = mat.clone();
-    for kp in keypoints {
-        let x = kp.point.x as c_int;
-        let y = kp.point.y as c_int;
-        draw_mat.ellipse(
-            Point2i::new(x, y),
-            Size2i::new((kp.size * 1.0).ceil() as i32, (kp.size * 0.5).ceil() as i32),
-            f64::from(kp.angle),
-            0.0,
-            360.0,
-        );
+    for (kps, (c1, c2)) in
+        [red_keypoints, green_keypoints, blue_keypoints]
+            .iter()
+            .zip(&[(&green, &blue), (&blue, &red), (&green, &red)])
+    {
+        for kp in kps {
+            let x = kp.point.x as c_int;
+            let y = kp.point.y as c_int;
+            c1.ellipse(
+                Point2i::new(x, y),
+                Size2i::new((kp.size * 1.0).ceil() as i32, (kp.size * 0.5).ceil() as i32),
+                f64::from(kp.angle),
+                0.0,
+                360.0,
+            );
+            c2.ellipse(
+                Point2i::new(x, y),
+                Size2i::new((kp.size * 1.0).ceil() as i32, (kp.size * 0.5).ceil() as i32),
+                f64::from(kp.angle),
+                0.0,
+                360.0,
+            );
+        }
     }
+    let complete_buffer = izip!(
+        blue.data().iter().cloned(),
+        green.data().iter().cloned(),
+        red.data().iter().cloned()
+    )
+    .flat_map(|(b, g, r)| once(b).chain(once(g)).chain(once(r)))
+    .collect::<Vec<u8>>();
+    let draw_mat = Mat::from_buffer(blue.rows, blue.cols, mat.cv_type(), &complete_buffer);
+
     draw_mat.show("SIFT Points", 0).unwrap();
 }
