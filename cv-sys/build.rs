0edc5f45f76e1988484b6e6382f5f7ab6060816d
Geordon Worley <vadixidav@gmail.com>
2019-03-10 16:56:16 -0400

    cv-sys now actually builds with new C++ bindings!

--- cv-sys/build.rs
+++ cv-sys/build.rs
@@ -72,12 +72,21 @@ fn main() {
     let dst = config.build();
 
     // Link cvsys.
-    println!("cargo:rustc-link-search=native={}", dst.join("build").join("Debug").display());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        dst.join("build").join("Debug").display()
+    );
     println!("cargo:rustc-link-lib=static=cvsys");
 
-    let bindings = Builder::default().rustfmt_bindings(true).whitelist_function("cvsys::.*").whitelist_type("cvsys::.*")
-        .opaque_type("cv::.*").opaque_type("std::.*")
-        .blacklist_function("cv::.*").blacklist_function("std::.*");
+    let bindings = Builder::default()
+        .rustfmt_bindings(true)
+        .whitelist_recursively(false)
+        .whitelist_function("cvsys::.*")
+        .whitelist_type("cvsys::.*")
+        .opaque_type("cv::.*")
+        .opaque_type("std::.*")
+        .whitelist_type("cv::MouseCallback")
+        .whitelist_type("cv::text::classifier_type");
     let bindings = bindings.clang_args(core_modules.iter().map(|lib| {
         format!(
             "-I{}",
--- cv-sys/src/lib.rs
+++ cv-sys/src/lib.rs
@@ -8,60 +8,75 @@
 use std::ffi::CStr;
 use std::iter::FromIterator;
 
+// Include bindings.
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-// impl From<cvsys_EmptyResult> for std::result::Result<(), String> {
-//     fn from(e: cvsys_EmptyResult) -> Self {
-//         if unsafe { e.error.is_str() } {
-//             Err(unsafe { CStr::from_ptr(e.error.get_str()) }
-//                 .to_str()
-//                 .expect("got non-UTF8 error string from OpenCV")
-//                 .to_owned())
-//         } else {
-//             Ok(())
-//         }
-//     }
-// }
+// Create opaque types.
+pub enum cv_BOWKMeansTrainer {}
+pub enum cv_CascadeClassifier {}
+pub enum cv_HOGDescriptor {}
+pub enum cv_Mat {}
+pub enum cv_TermCriteria {}
+pub enum cv_VideoCapture {}
+pub enum cv_VideoWriter {}
+pub enum cv_DMatch {}
+pub enum cv_KeyPoint {}
+pub enum cv_Point {}
+pub enum cv_Rect {}
+pub enum std_string {}
 
-// impl<T> Into<std::result::Result<T, String>> for cvsys_Result<T> {
-//     fn into(self) -> std::result::Result<T, String> {
-//         if unsafe { self.error.is_str() } {
-//             Err(unsafe { CStr::from_ptr(self.error.get_str()) }
-//                 .to_str()
-//                 .expect("got non-UTF8 error string from OpenCV")
-//                 .to_owned())
-//         } else {
-//             Ok(self.value)
-//         }
-//     }
-// }
+impl From<cvsys_EmptyResult> for Result<(), String> {
+    fn from(e: cvsys_EmptyResult) -> Self {
+        if unsafe { e.error.is_str() } {
+            Err(unsafe { CStr::from_ptr(e.error.get_str()) }
+                .to_str()
+                .expect("got non-UTF8 error string from OpenCV")
+                .to_owned())
+        } else {
+            Ok(())
+        }
+    }
+}
 
-// impl<T> Into<std::option::Option<T>> for cvsys_COption<T> {
-//     fn into(self) -> std::option::Option<T> {
-//         if self.hasValue {
-//             Some(self.value)
-//         } else {
-//             None
-//         }
-//     }
-// }
+impl<T> Into<std::result::Result<T, String>> for cvsys_Result<T> {
+    fn into(self) -> Result<T, String> {
+        if unsafe { self.error.is_str() } {
+            Err(unsafe { CStr::from_ptr(self.error.get_str()) }
+                .to_str()
+                .expect("got non-UTF8 error string from OpenCV")
+                .to_owned())
+        } else {
+            Ok(self.value)
+        }
+    }
+}
 
-// impl<T, U> Into<Vec<U>> for cvsys_CVec<T>
-// where
-//     T: Into<U> + Clone,
-// {
-//     fn into(self) -> Vec<U> {
-//         Vec::from_iter(
-//             (0..self.size)
-//                 .map(|n| unsafe { &*self.array.add(n) })
-//                 .cloned()
-//                 .map(Into::into),
-//         )
-//     }
-// }
+impl<T> Into<Option<T>> for cvsys_COption<T> {
+    fn into(self) -> Option<T> {
+        if self.hasValue {
+            Some(self.value)
+        } else {
+            None
+        }
+    }
+}
 
-// impl<T> cvsys_CVec<T> {
-//     pub fn iter(&self) -> impl Iterator<Item = &T> {
-//         unsafe { std::slice::from_raw_parts(self.array, self.size).iter() }
-//     }
-// }
+impl<T, U> Into<Vec<U>> for cvsys_CVec<T>
+where
+    T: Into<U> + Clone,
+{
+    fn into(self) -> Vec<U> {
+        Vec::from_iter(
+            (0..self.size)
+                .map(|n| unsafe { &*self.array.add(n) })
+                .cloned()
+                .map(Into::into),
+        )
+    }
+}
+
+impl<T> cvsys_CVec<T> {
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        unsafe { std::slice::from_raw_parts(self.array, self.size).iter() }
+    }
+}
