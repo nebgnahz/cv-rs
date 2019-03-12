4a0ad788d2490c0fe32eb1e85150ca6c3a372a42
Marc Haubenstock <marchaubenstock@Marcs-MacBook-Pro.local>
2019-03-12 12:10:38 +0100

    rustfmt for example

--- examples/calc_gradient.rs
+++ examples/calc_gradient.rs
@@ -2,17 +2,14 @@ extern crate cv;
 
 use cv::highgui::*;
 
-fn main () {
-
-    let image_mat
-        = cv::Mat::from_path("assets/lenna.png", cv::imgcodecs::ImageReadMode::Grayscale);
+fn main() {
+    let image_mat = cv::Mat::from_path("assets/lenna.png", cv::imgcodecs::ImageReadMode::Grayscale);
 
     match image_mat {
         Ok(mat) => {
-            let scharr_x = mat.scharr(mat.depth,0,1,1.0,0.0,cv::BorderType::Default);
+            let scharr_x = mat.scharr(mat.depth, 0, 1, 1.0, 0.0, cv::BorderType::Default);
             scharr_x.show("Display", 0).unwrap()
-        },
+        }
         Err(e) => println!("error loading image header: {:?}", e),
     }
-
 }
