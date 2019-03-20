5fed40c07a7c0f7ec9dfb91e0d7a95bdd04ac209
Geordon Worley <vadixidav@gmail.com>
2019-03-19 22:30:44 -0400

    vastly simplify build.rs for all configurations

--- cv-sys/build.rs
+++ cv-sys/build.rs
@@ -3,7 +3,6 @@ extern crate cmake;
 
 use bindgen::Builder;
 use cmake::Config;
-use itertools::Itertools;
 use std::env;
 use std::path::PathBuf;
 
@@ -15,17 +14,35 @@ fn cmake_bool(flag: bool) -> &'static str {
     }
 }
 
+fn cmake_common(config: &mut Config, target_os: &str) {
+    // Make everything static.
+    config.define("BUILD_SHARED_LIBS", "OFF").profile("Release");
+
+    // Handle OS-specific requirements.
+    match target_os {
+        "linux" => {
+            // There may be issues with precompiled headers and binding generation.
+            config.define("ENABLE_PRECOMPILED_HEADERS", "OFF");
+        }
+        "windows" => {
+            // Really make sure we use static runtime.
+            config
+                .static_crt(true)
+                .define("CMAKE_C_FLAGS", "/MT")
+                .define("CMAKE_C_FLAGS_RELEASE", "/MT")
+                .define("CMAKE_CXX_FLAGS", "/MT")
+                .define("CMAKE_CXX_FLAGS_RELEASE", "/MT")
+                .define("BUILD_WITH_STATIC_CRT", "ON");
+        }
+        _ => {}
+    }
+}
+
 fn main() -> Result<(), std::io::Error> {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let feature_cuda = env::var("CARGO_FEATURE_CUDA").is_ok();
     let feature_system = env::var("CARGO_FEATURE_SYSTEM").is_ok();
-
-    // Get profile variables.
-    let (configuration, lib_postfix) = match env::var("PROFILE").unwrap().as_str() {
-        "debug" => ("Debug", if target_os == "windows" {"d"} else {""}),
-        "release" => ("Release", ""),
-        _ => panic!("unknown PROFILE env var from Cargo"),
-    };
 
     let mut used_core_modules = vec![
         "core",
@@ -53,185 +70,126 @@ fn main() -> Result<(), std::io::Error> {
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     // Path to contrib modules.
     let contrib_modules_path = manifest_dir.join("opencv_contrib").join("modules");
-    // Global configuration for OpenCV build.
-    let mut config = Config::new(".");
 
-    // Set various cmake definitions.
-    config
-        // TODO: IPP creates some really annoying build issues on Windows.
-        // Eventually we need to fix it.
-        .define("CVSYS_SYSTEM_CV", cmake_bool(feature_system))
+    let (opencv_include_dir, opencv_lib_dir) = if feature_system {
+        let opencv_dir = env::var("OPENCV_DIR")
+            .unwrap_or_else(|_| panic!("OPENCV_DIR not set (set it to the directory the include folder is in)"));
+        let opencv_lib_dir = env::var("OPENCV_LIB")
+            .unwrap_or_else(|_| panic!("OPENCV_LIB not set (set it to where the OpenCV libs are)"));
+        (PathBuf::from(opencv_dir).join("include"), PathBuf::from(opencv_lib_dir))
+    } else {
+        // Global configuration for OpenCV build.
+        let mut opencv_config = Config::new("opencv");
+
+        // Apply common CMake options.
+        cmake_common(&mut opencv_config, &target_os);
+
+        // Set which modules we want to build.
+        for module in &all_used_modules {
+            opencv_config.define(format!("BUILD_opencv_{}", module), "ON");
+        }
+
+        let out_lib_dir = out_dir
+            .join("lib")
+            .to_str()
+            .expect("path must support unicode")
+            .replace("\\", "/");
+
+        let out_bin_dir = out_dir
+            .join("bin")
+            .to_str()
+            .expect("path must support unicode")
+            .replace("\\", "/");
+
+        // Set various cmake definitions for OpenCV.
+        opencv_config
+            .define("OPENCV_LIB_INSTALL_PATH", &out_lib_dir)
+            .define("OPENCV_3P_LIB_INSTALL_PATH", &out_lib_dir)
+            .define("OPENCV_BIN_INSTALL_PATH", &out_bin_dir)
+            .define("WITH_CUDA", cmake_bool(feature_cuda))
+            // TODO: IPP creates some really annoying build issues on Windows.
+            // Eventually we need to fix it.
+            .define("WITH_IPP", "OFF")
+            .define("BUILD_IPP_IW", "OFF")
+            .define("BUILD_opencv_apps", "OFF")
+            .define("BUILD_opencv_java_bindings_generator", "OFF")
+            .define("BUILD_opencv_js", "OFF")
+            .define("BUILD_opencv_python_bindings_generator", "OFF")
+            .define("BUILD_opencv_world", "OFF")
+            .define("INSTALL_CREATE_DISTRIB", "ON")
+            .define("INSTALL_PYTHON_EXAMPLES", "OFF")
+            .define("INSTALL_C_EXAMPLES", "OFF")
+            .define("OPENCV_ENABLE_NONFREE", "ON")
+            .define("OPENCV_EXTRA_MODULES_PATH", contrib_modules_path.into_os_string())
+            .define("BUILD_opencv_python", "OFF")
+            .define("BUILD_opencv_python2", "OFF")
+            .define("BUILD_opencv_python3", "OFF")
+            .define("BUILD_TESTS", "OFF")
+            .define("BUILD_PERF_TESTS", "OFF")
+            .define("BUILD_DOCS", "OFF")
+            .define("BUILD_EXAMPLES", "OFF");
+
+        let dst = opencv_config.build();
+
+        println!("cargo:rustc-link-lib=static=cvsys");
+
+        (dst.join("include"), dst.join("lib"))
+    };
+
+    // Global configuration for our native wrapper called cvsys.
+    let mut cvsys_config = Config::new(".");
+
+    // Apply common CMake options.
+    cmake_common(&mut cvsys_config, &target_os);
+
+    // Set modules used and opencv include dir for cvsys.
+    cvsys_config
         .define("CV_CORE_MODULES", used_core_modules.join(";"))
         .define("CV_CONTRIB_MODULES", used_contrib_modules.join(";"))
-        .define("WITH_IPP", "OFF")
-        .define("BUILD_IPP_IW", "OFF")
-        .define("BUILD_opencv_apps", "OFF")
-        .define("BUILD_opencv_java_bindings_generator", "OFF")
-        .define("BUILD_opencv_js", "OFF")
-        .define("BUILD_opencv_python_bindings_generator", "OFF")
-        .define("BUILD_opencv_world", "OFF")
-        .define("INSTALL_CREATE_DISTRIB", "ON")
-        .define("BUILD_SHARED_LIBS", "OFF")
-        .define("CMAKE_BUILD_TYPE", configuration.to_ascii_uppercase())
-        .define("INSTALL_PYTHON_EXAMPLES", "OFF")
-        .define("INSTALL_C_EXAMPLES", "OFF")
-        .define("OPENCV_ENABLE_NONFREE", "ON")
-        .define("OPENCV_EXTRA_MODULES_PATH", contrib_modules_path.into_os_string())
-        .define(
-            "WITH_CUDA",
-            cmake_bool(feature_cuda),
-        )
-        .define("BUILD_opencv_python", "OFF")
-        .define("BUILD_opencv_python2", "OFF")
-        .define("BUILD_opencv_python3", "OFF")
-        .define("BUILD_TESTS", "OFF")
-        .define("BUILD_PERF_TESTS", "OFF")
-        .define("BUILD_DOCS", "OFF")
-        .define("BUILD_EXAMPLES", "OFF");
+        .define("CVSYS_INCLUDE_DIR", &opencv_include_dir);
 
-    if feature_system {
-        let opencv_lib_dir = env::var("OPENCV_LIB").unwrap_or_else(|_| panic!("OPENCV_LIB not set (set it to where the OpenCV libs are)"));
-        let opencv_dir = env::var("OPENCV_DIR").unwrap_or_else(|_| panic!("OPENCV_DIR not set (set it to the directory the include folder is in)"));
-        let opencv_include_dir = PathBuf::from(opencv_dir).join("include");
-        config
-            .define("CVSYS_LIB_DIR", opencv_lib_dir)
-            .define("CVSYS_INCLUDE_DIR", opencv_include_dir);
+    // Build cvsys.
+    let dst = cvsys_config.build();
+
+    // Link cvsys.
+    println!("cargo:rustc-link-lib=static=cvsys");
+
+    // Add search path for cvsys.
+    println!("cargo:rustc-link-search={}", dst.join("lib").display());
+
+    // Link all dependencies.
+    for entry in opencv_lib_dir.read_dir()? {
+        let entry = entry?;
+        if entry.path().extension().map(|os| os == "cmake").unwrap_or(false) {
+            continue;
+        }
+        let libname = entry
+            .path()
+            .file_stem()
+            .unwrap()
+            .to_str()
+            .expect("OpenCV lib names must be unicode")
+            .to_owned();
+        println!("cargo:rustc-link-lib=static={}", libname);
     }
 
-    // Set which modules we want to build.
-    for module in &all_used_modules {
-        config.define(format!("BUILD_opencv_{}", module), "ON");
-    }
+    // Add search path for OpenCV libs.
+    println!("cargo:rustc-link-search={}", opencv_lib_dir.display());
 
-    // Handle OS-specific requirements.
+    // Handle OS-specific linker requirements.
     match target_os.as_str() {
         "linux" => {
             println!("cargo:rustc-link-lib=gomp");
             println!("cargo:rustc-link-lib=stdc++");
-
-            config.define("ENABLE_PRECOMPILED_HEADERS", "OFF");
         }
         "windows" => {
             println!("cargo:rustc-link-lib=comdlg32");
             println!("cargo:rustc-link-lib=Vfw32");
             println!("cargo:rustc-link-lib=Ole32");
             println!("cargo:rustc-link-lib=OleAut32");
-
-            config.static_crt(true)
-                .define("CMAKE_C_FLAGS", "/MT")
-                .define("CMAKE_C_FLAGS_DEBUG", "/MT")
-                .define("CMAKE_C_FLAGS_RELEASE", "/MT")
-                .define("CMAKE_C_FLAGS_MINSIZEREL", "/MT")
-                .define("CMAKE_C_FLAGS_RELWITHDEBINFO", "/MT")
-                .define("CMAKE_CXX_FLAGS", "/MT")
-                .define("CMAKE_CXX_FLAGS_DEBUG", "/MT")
-                .define("CMAKE_CXX_FLAGS_RELEASE", "/MT")
-                .define("CMAKE_CXX_FLAGS_MINSIZEREL", "/MT")
-                .define("CMAKE_CXX_FLAGS_RELWITHDEBINFO", "/MT")
-                .define("BUILD_WITH_STATIC_CRT", "ON");
         }
+        // Please send in a PR for your favorite platform!
         _ => {}
-    }
-
-    // Build OpenCV and add it to cargo.
-    let dst = config.build();
-
-    // Link cvsys.
-    println!("cargo:rustc-link-lib=static=cvsys");
-
-    if feature_system {
-        // Add cvsys path.
-        if target_os == "windows" {
-                println!(
-                    "cargo:rustc-link-search={}",
-                    dst.join("build").join(configuration).display()
-                );
-        } else {
-            println!(
-                "cargo:rustc-link-search={}",
-                dst.join("build").display()
-            );
-        }
-        // Add opencv libs path.
-        let opencv_lib_dir = env::var("OPENCV_LIB").unwrap_or_else(|_| panic!("OPENCV_LIB not set (set it to where the OpenCV libs are)"));
-        println!(
-            "cargo:rustc-link-search={}",
-            opencv_lib_dir
-        );
-        // Link all modules used.
-        let opencv_vers = env::var("OPENCV_VERS").unwrap_or_else(|_| panic!("OPENCV_VERS not set (set it to the number appended to lib names, e.g. 341)"));
-        for module in &all_used_modules {
-            println!(
-                "cargo:rustc-link-lib=static=opencv_{}{}",
-                module,
-                opencv_vers
-            );
-        }
-    } else {
-        // Load link_libs_list.txt to get list of all static libs to link to.
-        // This is generated in the CMakeLists.txt.
-        let libs_list: Vec<PathBuf> = std::fs::read_to_string(dst.join("build").join("link_libs_list.txt"))
-            .expect("CMakeLists.txt didn't produce libs list")
-            .replace("$(Configuration)", configuration)
-            .split(';')
-            .map(Into::into)
-            .collect();
-
-        // This contains the raw lib names.
-        let deduped_lib_names: Vec<String> = libs_list
-            .iter()
-            .map(|p| p.file_stem().expect("expected only files in libs list").to_str().expect("OpenCV lib names must be valid UTF-8").to_owned())
-            .unique()
-            .collect();
-
-        // This contains the lib search paths.
-        let deduped_lib_search_paths: Vec<PathBuf> = libs_list
-            .iter()
-            .map(|p| p.parent().expect("expected libs to be in a directory").to_path_buf())
-            .unique()
-            .collect();
-
-        // Change linker paths based on OS.
-        match target_os.as_str() {
-            "windows" => {
-                // Add cvsys path.
-                println!(
-                    "cargo:rustc-link-search={}",
-                    dst.join("build").join(configuration).display()
-                );
-
-                // Add all the static libs that need to be linked to Cargo, adding
-                // postfixes as necessary (on Windows a 'd' is appended to libs in debug mode).
-                for libname in deduped_lib_names {
-                    println!(
-                        "cargo:rustc-link-lib=static={}{}",
-                        libname,
-                        lib_postfix
-                    );
-                }
-            }
-            _ => {
-                // Add cvsys path.
-                println!(
-                    "cargo:rustc-link-search={}",
-                    dst.join("build").display()
-                );
-
-                // Add all the static libs that need to be linked to Cargo, adding
-                // postfixes as necessary (on Windows a 'd' is appended to libs in debug mode).
-                for libname in deduped_lib_names {
-                    println!(
-                        "cargo:rustc-link-lib=static={}",
-                        &libname[3..]
-                    );
-                }
-            },
-        };
-
-        // Add all cvsys dependencies' paths.
-        for libpath in deduped_lib_search_paths {
-            println!("cargo:rustc-link-search={}", libpath.display());
-        }
     }
 
     // Set up bindgen to generate bindings from our C++ wrapper.
@@ -250,49 +208,12 @@ fn main() -> Result<(), std::io::Error> {
         .opaque_type("std::.*")
         .whitelist_type("cv::MouseCallback")
         .whitelist_type("cv::text::classifier_type");
-    
-    // Change bindgen settings based on OS.
-    let bindings = match target_os.as_str() {
-        "linux" => {
-            bindings.clang_args(&["-x", "c++", "-std=c++14", "-stdlib=libc++"])
-        }
-        _ => bindings,
-    };
 
-    let bindings = if feature_system {
-        let opencv_dir = env::var("OPENCV_DIR").unwrap_or_else(|_| panic!("OPENCV_DIR not set (set it to the directory the include folder is in)"));
-        let opencv_include_dir = PathBuf::from(opencv_dir).join("include");
-        bindings.clang_arg(format!("-I{}", opencv_include_dir.display()))
-    } else {
-        // Add core module includes.
-        let bindings = bindings.clang_args(used_core_modules.iter().map(|lib| {
-            format!(
-                "-I{}",
-                manifest_dir
-                    .join("opencv")
-                    .join("modules")
-                    .join(lib)
-                    .join("include")
-                    .display()
-            )
-        }));
+    // Add some common flags.
+    let bindings = bindings.clang_args(&["-x", "c++", "-std=c++14"]);
 
-        // Add contrib module includes.
-        let bindings = bindings.clang_args(used_contrib_modules.iter().map(|lib| {
-            format!(
-                "-I{}",
-                manifest_dir
-                    .join("opencv_contrib")
-                    .join("modules")
-                    .join(lib)
-                    .join("include")
-                    .display()
-            )
-        }));
-
-        // Add include from root of build folder.
-        bindings.clang_arg(format!("-I{}", dst.join("include").display()))
-    };
+    // Add OpenCV include directory.
+    let bindings = bindings.clang_arg(format!("-I{}", opencv_include_dir.display()));
 
     // Add all wrapper headers.
     let bindings = all_used_modules
@@ -300,8 +221,14 @@ fn main() -> Result<(), std::io::Error> {
         .map(|lib| format!("native/{}.hpp", lib))
         .fold(bindings, Builder::header);
 
+    // Change bindgen settings based on OS.
+    let bindings = match target_os.as_str() {
+        "linux" => bindings.clang_arg("-stdlib=libc++"),
+        _ => bindings,
+    };
+
     // Finally generate the bindings.
-    let bindings = bindings.generate().expect("Unable to generate bindings");
+    let bindings = bindings.generate().expect("bindgen was unable to generate bindings");
 
     // Write the bindings in the build directory.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
